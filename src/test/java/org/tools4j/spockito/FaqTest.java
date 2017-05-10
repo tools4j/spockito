17229de0b4baffb0690df36f9dd3429fd0e8b9e2
Marco Terzer <terzerm@gmail.com>
2017-05-10 21:54:31 +1000

    Added Issue #6 example to FaqTest to illustrate how to deal with non-printable and whitespace characters

--- src/test/java/org/tools4j/spockito/FaqTest.java
+++ src/test/java/org/tools4j/spockito/FaqTest.java
@@ -23,7 +23,10 @@
  */
 package org.tools4j.spockito;
 
-import org.junit.*;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.math.BigInteger;
@@ -41,11 +44,12 @@ public class FaqTest {
 
     @AfterClass
     public static void assertSpecialStringTestCount() {
-        Assume.assumeTrue("Assume that testSpecialStrings() has been run at least once", SPECIAL_STRING_COUNT.get() > 0);
-        Assert.assertEquals("Expected 4 runs of testSpecialStrings()", 4, SPECIAL_STRING_COUNT.get());
+        if (SPECIAL_STRING_COUNT.get() > 0) {
+            Assert.assertEquals("Expected 4 runs of testSpecialStrings()", 4, SPECIAL_STRING_COUNT.get());
+        }
     }
 
-    /** Question related to issue #1 */
+    /** Example related to issue #1 */
     @Test(expected = IllegalArgumentException.class)
     @Spockito.Unroll({
             "|Input|",
@@ -63,7 +67,7 @@ public class FaqTest {
         Assert.fail("should have thrown an exception");
     }
 
-    /** Question related to issue #2 */
+    /** Example related to issue #2 */
     @Test
     @Spockito.Unroll({
             "|Input|Location ID|Event ID|",
@@ -73,6 +77,85 @@ public class FaqTest {
     public void testWithPipe(String input, BigInteger locationId, BigInteger eventId) {
         Assert.assertTrue(!input.contains("\\"));
         Assert.assertTrue(input.contains("|") || input.startsWith("123") && input.endsWith("321"));
+    }
+
+    /** Example related to issue #6 */
+    @Test
+    @Spockito.Unroll({
+            "|Character|",
+            "|'\u0001'|",
+            "|'\u0002'|",
+            "|'\u0003'|",
+            "|'\u0004'|",
+            "|'\u0005'|",
+            "|'\u0006'|",
+            "|'\u0007'|",
+            "|'\u0008'|",
+            "|'\u0009'|",
+            "|'\u000b'|",
+            "|'\u000c'|",
+            "|'\u000e'|",
+            "|'\u000f'|",
+            "|'\u0010'|",
+            "|'\u0011'|",
+            "|'\u0012'|",
+            "|'\u0013'|",
+            "|'\u0014'|",
+            "|'\u0015'|",
+            "|'\u0016'|",
+            "|'\u0017'|",
+            "|'\u0018'|",
+            "|'\u0019'|",
+            "|'\u001a'|",
+            "|'\u001b'|",
+            "|'\u001c'|",
+            "|'\u001d'|",
+            "|'\u001e'|",
+            "|'\u001f'|",
+            "|'\u0020'|"
+    })
+    public void testNonPrintableOrWhitespaceChar(char ch) {
+        Assert.assertTrue("Char should be non-printable or whitespace", ch <= '\u0021');
+    }
+
+    /** Example related to issue #6 */
+    @Test
+    @Spockito.Unroll({
+            "|Character|",
+            "|'\u0001'|",
+            "|'\u0002'|",
+            "|'\u0003'|",
+            "|'\u0004'|",
+            "|'\u0005'|",
+            "|'\u0006'|",
+            "|'\u0007'|",
+            "|'\u0008'|",
+            "|'\u0009'|",
+            "|'\u000b'|",
+            "|'\u000c'|",
+            "|'\u000e'|",
+            "|'\u000f'|",
+            "|'\u0010'|",
+            "|'\u0011'|",
+            "|'\u0012'|",
+            "|'\u0013'|",
+            "|'\u0014'|",
+            "|'\u0015'|",
+            "|'\u0016'|",
+            "|'\u0017'|",
+            "|'\u0018'|",
+            "|'\u0019'|",
+            "|'\u001a'|",
+            "|'\u001b'|",
+            "|'\u001c'|",
+            "|'\u001d'|",
+            "|'\u001e'|",
+            "|'\u001f'|",
+            "|'\u0020'|"
+    })
+    public void testNonPrintableOrWhitespaceCharsAsString(String s) {
+        Assert.assertEquals("String length should be 1", 1, s.length());
+        Assert.assertTrue("Char should be non-printable or whitespace", s.charAt(0) <= '\u0021');
     }
 
 }
