6bd7563f5e536a687eb57084a235983405dcfe3b
Marco Terzer <terzerm@gmail.com>
2021-01-27 01:23:23 +1100

    Update licence/copyright year

--- spockito-junit4/src/test/java/org/tools4j/spockito/Operation.java
+++ spockito-junit4/src/test/java/org/tools4j/spockito/Operation.java
@@ -1,7 +1,7 @@
 /**
  * The MIT License (MIT)
  *
- * Copyright (c) 2017-2020 tools4j.org (Marco Terzer)
+ * Copyright (c) 2017-2021 tools4j.org (Marco Terzer)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
--- spockito-junit5/src/test/java/org/tools4j/spockito/jupiter/TableSourceTest.java
+++ spockito-junit5/src/test/java/org/tools4j/spockito/jupiter/TableSourceTest.java
@@ -24,7 +24,7 @@
 package org.tools4j.spockito.jupiter; /**
  * The MIT License (MIT)
  *
- * Copyright (c) 2017-2020 tools4j.org (Marco Terzer)
+ * Copyright (c) 2017-2021 tools4j.org (Marco Terzer)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
